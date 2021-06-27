89bd782bdd2adff26e0acb4fd0c4e1f2fa3f5fce
FErArg <ferarg@ferarg.com>
2021-06-27 19:00:20 +0200

    sync

--- Scripts/randomPass.ps1
+++ Scripts/randomPass.ps1
@@ -1,9 +1,26 @@
 Add-Type -AssemblyName 'System.Web'
 
-$minLength = 5 ## characters
-$maxLength = 10 ## characters
-$length = Get-Random -Minimum $minLength -Maximum $maxLength
-$nonAlphaChars = 5
-$password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+function New-RandomPassword {
+    param(
+        [Parameter()]
+        [int]$MinimumPasswordLength = 5,
+        [Parameter()]
+        [int]$MaximumPasswordLength = 10,
+        [Parameter()]
+        [int]$NumberOfAlphaNumericCharacters = 5,
+        [Parameter()]
+        [switch]$ConvertToSecureString
+    )
+    
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
+    if ($ConvertToSecureString.IsPresent) {
+        ConvertTo-SecureString -String $password -AsPlainText -Force
+    } else {
+        $password
+    }
+}
 
-Write-Host $password
+$password2 = New-RandomPassword -MinimumPasswordLength 10 -MaximumPasswordLength 15 -NumberOfAlphaNumericCharacters 6 -ConvertToSecureString
+Write-Host $password2
