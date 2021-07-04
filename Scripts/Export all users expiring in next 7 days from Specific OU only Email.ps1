1f3467778c6c93f69d955a5a5ff29ea6d57da857
FErArg <ferarg@ferarg.com>
2021-07-04 13:01:43 +0200

    cambios

--- Scripts/Export all users expiring in next 7 days from Specific OU only Email.ps1
+++
@@ -1,5 +0,0 @@
-$OUpath = 'OU=Levante,OU=Concesiones,DC=VGRS,DC=local'
-Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -SearchBase $OUpath –Properties "DisplayName", "mail", "msDS-UserPasswordExpiryTimeComputed" | where { 
-$diff = New-TimeSpan ([datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")) (Get-Date)
-$diff.Days -le 7 -and $diff.Days -ge 0
-} | select "DisplayName","mail",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-csv -Path c:\temp\user_pass_expiring_from_ou.csv
