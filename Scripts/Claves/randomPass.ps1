b94c81c700aaabc134a82d66b04ea9dc88485e34
FErArg <47794292+FErArg@users.noreply.github.com>
2021-07-05 14:29:07 +0200

    Update randomPass.ps1

--- Scripts/Claves/randomPass.ps1
+++ Scripts/Claves/randomPass.ps1
@@ -21,3 +21,37 @@ For ($i=1; $i -lt 3; $i++){
     $clave += $palabra
 }
 Write-Host $clave
+
+$usuario = Read-Host -Prompt 'Usuario del AD'
+
+Add-Type -AssemblyName 'System.Web'
+$nuevaClave = $clave | ConvertTo-SecureString -AsPlainText -Force
+
+
+$outlook = new-object -comobject outlook.application
+$email = $outlook.CreateItem(0)
+$email.To = "fernando.rodriguez.ext@vwgroupretail.es"
+$email.Cc = "soporte.levante@vwgroupretail.es" 
+$email.Subject = "$Name - Aviso de Contraseña próxima a Caducar"
+$email.Body = "Buenas,
+			
+	Nueva clave:
+    
+    USUARIO; $usuario
+    CLAVE: $clave
+    
+	Para asistencia t�cnica, utilice siempre el Formulario Contacto - https://bit.ly/3lTg7xS 
+	
+    Atentamente,
+	
+    Fernando A. Rodríguez Mallou
+	Servicio IT Externo
+	Mvl +34 659 69 20 48
+	Ext 2973
+	fernando.rodriguez.ext@vwgroupretail.es
+	soporte.levante@vwgroupretail.es
+	" 
+					
+$email.Send()
+
+start powershell Set-ADAccountPassword -Identity $usuario -NewPassword $nuevaClave -Reset -credential ""
