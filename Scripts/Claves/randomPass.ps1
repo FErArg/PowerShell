a86ab8450692d036ea3525d0addaac0d58d8a290
FErArg <47794292+FErArg@users.noreply.github.com>
2021-07-05 13:48:03 +0200

    Add files via upload

---
+++ Scripts/Claves/randomPass.ps1
@@ -0,0 +1,24 @@
+
+[string[]]$palabras = Get-Content -Path 'palabras.txt'
+[string[]]$numeros = Get-Content -Path 'numeros.txt'
+[string[]]$simbolos = Get-Content -Path 'simbolos.txt'
+
+For ($i=1; $i -lt 4; $i++){
+    Write-Host $i
+    if( $i -Eq '2'){
+        $item=(Get-Random -Maximum ([array]$numeros).count)
+        $numero=$numeros[$item]
+        $clave += $numero
+        <# Write-Host $numero #>
+    }
+    if( $i -Eq '3'){
+        $item=(Get-Random -Maximum ([array]$simbolos).count)
+        $simbolo=$simbolos[$item]
+        $clave += $simbolo
+        <# Write-Host $numero #>
+    }
+    $item=(Get-Random -Maximum ([array]$palabras).count)
+    $palabra=$palabras[$item]
+    $clave += $palabra
+}
+Write-Host $clave
