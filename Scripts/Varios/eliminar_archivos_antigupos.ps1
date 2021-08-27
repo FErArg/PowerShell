3e109bcc81986fca51a637548832b8b16a95767d
FErArg <47794292+FErArg@users.noreply.github.com>
2021-08-27 13:15:28 +0200

    Update eliminar_archivos_antigupos.ps1

--- Scripts/Varios/eliminar_archivos_antigupos.ps1
+++ Scripts/Varios/eliminar_archivos_antigupos.ps1
@@ -1,1 +1,8 @@
+$limit = (Get-Date).AddDays(-15)
+$path = "D:\Datos\escaner_sedes\30082\Garantias"
 
+# Delete files older than the $limit.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+
+# Delete any empty directories left behind after deleting the old files.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
